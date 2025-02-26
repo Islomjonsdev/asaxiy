f7ef16ad6b43ce480ba4f4f192cb2293dc6ec2ce
Islomjonsdev <islomjon@immigrateable.com>
2025-02-26 23:59:11 +0500

    fixed

--- components/shared/Header/MainHeader/MainHeader.jsx
+++ components/shared/Header/MainHeader/MainHeader.jsx
@@ -7,10 +7,12 @@ import cartIcon from "../../../../public/svg/cart.svg";
 import likeIcon from "../../../../public/svg/heart.svg";
 import compareIcon from "../../../../public/svg/comparison.svg";
 import logoutIcon from "../../../../public/svg/logout.svg";
+import searchIcon from "../../../../public/svg/search.svg";
+import photoIcon from "../../../../public/svg/photo.svg";
 
 const MainHeader = () => {
   return (
-    <div className="flex items-center gap-[56px] mb-[12px]">
+    <div className="flex items-center gap-[40px] mb-[12px] relative">
       <Link href={"/"}>
         <Image
           src={mainHeaderLogo}
@@ -19,8 +21,26 @@ const MainHeader = () => {
           alt="mainHeaderLogo"
         />
       </Link>
-      <Input variant="formInput" placeholder="Product search"/>
-      <div className=" flex item-center gap-[20px]">
+      <Input
+        className="relative"
+        variant="formInput"
+        placeholder="Product search"
+      />
+      <Image
+        className="absolute top-[12px] left-[18%]"
+        src={searchIcon}
+        width={24}
+        height={24}
+        alt="searchIcon"
+      />
+      <Image
+        className="absolute top-[12px] right-[33%]"
+        src={photoIcon}
+        width={24}
+        height={24}
+        alt="photoIcon"
+      />
+      <div className=" flex item-center gap-[24px]">
         <Link
           className="flex flex-col items-center font-normal text-[15px] leading-[20px] text-[#5C596D]"
           href={"/"}
